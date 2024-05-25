40d9c2323bd0f65580153237f914427088383b14
mthunzi gcwensa <mthunzigcwensa659@gmail.com>
2024-05-25 23:11:59 +0200

    Shopping Cart API Controller

---
+++ onlineStore.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using onlineStore.Services.ShoppingCartAPI.Data;
+using onlineStore.Services.ShoppingCartAPI.Models.Dto;
+
+namespace onlineStore.Services.ShoppingCartAPI.Controllers
+{
+    [Route("api/cart")]
+    [ApiController]
+    public class CartAPIController : ControllerBase
+    {
+        private ResponseDto _response;
+        private IMapper _mapper;
+        private readonly AppDbContext _db;
+
+        public CartAPIController(AppDbContext db,
+            IMapper mapper)
+        {
+            _db = db;
+            this._response = new ResponseDto();
+            _mapper = mapper;
+        }
+
+        [HttpPost("CartUpsert")]
+        public async Task<ResponseDto> CartUpsert(CartDto cartDto)
+        {
+
+
+        }
+
+
+    }
+}
