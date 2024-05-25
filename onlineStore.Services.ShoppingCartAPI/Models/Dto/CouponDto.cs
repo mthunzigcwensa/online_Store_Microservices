ba712fe5c85b2184dbabbdb8086ba7da24b28cc6
mthunzi gcwensa <mthunzigcwensa659@gmail.com>
2024-05-25 22:42:44 +0200

    Cart Model and DTO's

---
+++ onlineStore.Services.ShoppingCartAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,10 @@
+﻿namespace onlineStore.Services.ShoppingCartAPI.Models.Dto
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
