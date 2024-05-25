771ba0d78bf71c1822e78000df00a753e22320cc
mthunzi gcwensa <mthunzigcwensa659@gmail.com>
2024-05-25 23:05:52 +0200

    api set-up

---
+++ onlineStore.Services.ShoppingCartAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using onlineStore.Services.ShoppingCartAPI.Models;
+
+namespace onlineStore.Services.ShoppingCartAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+    }
+}
---
+++ onlineStore.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using onlineStore.Services.ShoppingCartAPI.Models;
+using onlineStore.Services.ShoppingCartAPI.Models.Dto;
+
+namespace onlineStore.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+            });
+            return mappingConfig;
+        }
+    }
+}
