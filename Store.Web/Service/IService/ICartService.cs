1b7072591d6d29d8ef62fad744065f4d41d06a01
mthunzi gcwensa <mthunzigcwensa659@gmail.com>
2024-05-26 02:13:24 +0200

    Adding Cart Service

---
+++ Store.Web/Service/IService/ICartService.cs
@@ -0,0 +1,12 @@
+﻿using Store.Web.Models;
+
+namespace Store.Web.Service.IService
+{
+    public interface ICartService
+    {
+        Task<ResponseDto?> GetCartByUserIdAsnyc(string userId);
+        Task<ResponseDto?> UpsertCartAsync(CartDto cartDto);
+        Task<ResponseDto?> RemoveFromCartAsync(int cartDetailsId);
+        Task<ResponseDto?> ApplyCouponAsync(CartDto cartDto);
+    }
+}
